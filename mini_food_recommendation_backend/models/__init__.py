ab7ee0f9021d05ab9e449826ef4181d1c8db7b09
Alhadji Oumate <alhadjioum0@gmail.com>
2025-06-11 10:30:20 +0100

    refactor: importing weekly plans and buffet management db models

--- mini_food_recommendation_backend/models/__init__.py
+++ mini_food_recommendation_backend/models/__init__.py
@@ -2,5 +2,7 @@ from .person import Person
 from .food import Food, Ingredient
 from .food_consumption import FoodConsumption
 from .food_image import FoodImage
+from .weekly_plan import WeeklyPlan
+from .buffet_manage import BuffetPlan, BuffetFood
 
-__all__ = ['Person', 'Food', 'Ingredient', 'FoodConsumption', 'FoodImage']
+__all__ = ['Person', 'Food', 'Ingredient', 'FoodConsumption', 'FoodImage', 'WeeklyPlan', 'BuffetPlan', 'BuffetFood']
