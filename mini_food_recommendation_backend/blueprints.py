78c389f0761fecbaf5d29c1b82ffd060c6d88adb
Alhadji Oumate <alhadjioum0@gmail.com>
2025-06-11 10:27:26 +0100

    feat: registering the weekly plan and buffer blueprints

--- mini_food_recommendation_backend/blueprints.py
+++ mini_food_recommendation_backend/blueprints.py
@@ -3,6 +3,8 @@ from .routes.persons import persons_bp
 from .routes.food_recommendation import rec_bp as food_recommendation_bp
 from .routes.allergy_probability import rec_bp as allergy_probability_bp
 from .routes.food_consumptions import food_consumptions_bp
+from .routes.weekly_plan import weekly_plan_bp
+from .routes.buffet_manage import buffet_bp
 
 def register_blueprints(app):
     app.register_blueprint(foods_bp)
@@ -10,3 +12,5 @@ def register_blueprints(app):
     app.register_blueprint(food_recommendation_bp)
     app.register_blueprint(allergy_probability_bp)
     app.register_blueprint(food_consumptions_bp)
+    app.register_blueprint(weekly_plan_bp)
+    app.register_blueprint(buffet_bp)
